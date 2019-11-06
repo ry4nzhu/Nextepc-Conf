af69e4f0a7b6e92e6f8c6d296fb647eed52e8879
Ruiyang Zhu <ryanzhu@umich.edu>
2019-11-06 15:02:15 -0500

    add conf file in script epc

--- start_epc.sh
+++ start_epc.sh
@@ -35,7 +35,7 @@ sudo systemctl restart systemd-networkd
 sudo ip addr add 192.168.0.1/24 dev pgwtun
 sudo ip link set up dev pgwtun
 sudo iptables -t nat -A POSTROUTING -o `cat /var/emulab/boot/controlif` -j MASQUERADE
-cd /opt/nextepc/install/etc/nextepc
+mv /opt/nextepc.conf /opt/nextepc/install/etc/nextepc
 
 # edit conf files
 
